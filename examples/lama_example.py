2bef33761e7e7d47f8504570b764e40ade1851e7
pacificus <masterkristall@gmail.com>
2023-04-03 22:54:31 +0700

    fix: change code style

--- examples/lama_example.py
+++ examples/lama_example.py
@@ -10,20 +10,18 @@ from deployme import cook
 # noinspection PyPep8Naming
 def main():
     data = load_iris(as_frame=True)
-    data = pd.concat([data['data'], data['target']], axis=1, ignore_index=True)
+    data = pd.concat([data["data"], data["target"]], axis=1, ignore_index=True)
     data = data.rename(columns={i: str(i) for i in data.columns})
-    X_train, X_test = train_test_split(
-        data, stratify=data["4"], test_size=0.2
-    )
+    X_train, X_test = train_test_split(data, stratify=data["4"], test_size=0.2)
     # create model instance
     lama = TabularAutoML(
-        task=Task(name='multiclass'),
+        task=Task(name="multiclass"),
         timeout=300,
     )
     # fit model
     lama.fit_predict(
         X_train,
-        roles={'target': '4'},
+        roles={"target": "4"},
     )
 
     cook(strategy="docker", model=lama, verbose=True)
--- tests/functional/test_lama.py
+++ tests/functional/test_lama.py
@@ -14,26 +14,25 @@ log = logging.getLogger(__name__)
 
 
 @pytest.mark.skipif(
-    not is_package_installed("lightautoml"), reason="LightAutoML is not installed"
+    not is_package_installed("lightautoml"),
+    reason="LightAutoML is not installed",
 )
 def test_cook_lama():
     from lightautoml.automl.presets.tabular_presets import TabularAutoML
     from lightautoml.tasks import Task
 
     data = load_iris(as_frame=True)
-    data = pd.concat([data['data'], data['target']], axis=1, ignore_index=True)
+    data = pd.concat([data["data"], data["target"]], axis=1, ignore_index=True)
     data = data.rename(columns={i: str(i) for i in data.columns})
-    X_train, X_test = train_test_split(
-        data, stratify=data["4"], test_size=0.2
-    )
+    X_train, X_test = train_test_split(data, stratify=data["4"], test_size=0.2)
     # create model instance
     lama = TabularAutoML(
-        task=Task(name='multiclass'),
+        task=Task(name="multiclass"),
         timeout=300,
     )
     lama.fit_predict(
         X_train,
-        roles={'target': '4'},
+        roles={"target": "4"},
     )
 
     cook(model=lama, strategy="local", backend="sanic", need_run=False)
